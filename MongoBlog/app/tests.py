9b590dc5132c4a7160a8337933630396deda4d4c
twincharged <joseph.cs.ritchie@gmail.com>
2015-04-05 12:50:31 -0700

    Update tests.py

--- MongoBlog/app/tests.py
+++ MongoBlog/app/tests.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 
 class SimpleTest(TestCase):
     def test_basic_division(self):
-        """
-        Tests that 2/1 always equals 2.
-        """
+        # Tests that 2/1 always equals 2.
+
         self.assertEqual(2/1, 2)
